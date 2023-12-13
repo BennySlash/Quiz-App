7d0b62ab05a12ce698f436838c10c57c05949199
BennyHabesha <biniyamtessema29@gmail.com>
2023-12-13 12:58:14 +0300

    initial commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+
+const Home = () => {
+  return (
+    <>
+      <Helmet>
+        <title>Quize App - Home</title>
+      </Helmet>
+    </>
+  );
+};
+
+export default Home;
