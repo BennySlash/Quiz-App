9a3f29080dceca8c4fae95c5b4a935b250cdecbd
BennyHabesha <biniyamtessema29@gmail.com>
2023-12-13 15:41:39 +0300

    second commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Helmet } from "react-helmet";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
@@ -7,6 +8,25 @@ const Home = () => {
       <Helmet>
         <title>Quize App - Home</title>
       </Helmet>
+      <div id="home">
+        <section>
+          <div>
+            <img src="./assets/img/gebeya-logo.png" alt="gebeya logo" c />
+          </div>
+          <h1>Quiz App</h1>
+          <div className="play-button-container">
+            <ul>
+              <li>
+                <Link to="/play/instructions">Play</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="auth-container">
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+          </div>
+        </section>
+      </div>
     </>
   );
 };
